b55de7ab06815df51b1b756c30930dd9d8059ca9
callmejeje <choije401@naver.com>
2023-06-24 21:37:11 +0900

    feat: 상품 조회, 작성 api 요청

--- src/apis/services/productService.ts
+++ src/apis/services/productService.ts
@@ -1,11 +1,25 @@
 import axiosInstance from "../config/axiosInstance";
 
-// 예시
-export const getUsers = async () => {
+export const getAllProducts = async () => {
   try {
-    const response = await axiosInstance.get("/users");
+    const response = await axiosInstance.get("/api/products");
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch users");
+  }
+};
+
+export const postProduct = async (data: FormData) => {
+  try {
+    const headers = {
+      "Content-Type": "multipart/form-data",
+    };
+
+    const response = await axiosInstance.post("/api/products", data, {
+      headers,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to post data");
   }
 };
