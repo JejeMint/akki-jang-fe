64223b4df0669843c782e84dc55142eb64597fe3
callmejeje <choije401@naver.com>
2023-06-24 21:38:09 +0900

    feat: Styled 헤더

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  border-bottom: 1px solid #ccc;
+  padding: 12px;
+`;
+
+export default Header;
