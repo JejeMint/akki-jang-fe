82250e103470a3985c16940e90c5349e83715da3
callmejeje <choije401@naver.com>
2023-08-10 09:38:50 +0900

    feat: 상품 작성 페이지 구현

--- src/pages/CreateProduct.tsx
+++ src/pages/CreateProduct.tsx
@@ -1,5 +1,68 @@
+import styled from "styled-components";
+import { postProduct } from "../apis/services/productService";
+import Form from "../components/forms/Form";
+import ContentLayout from "../components/layout/ContentLayout";
+import Header from "../components/layout/Header";
+import { createProductFormFields } from "../constants/formFields/createProductFormFields";
+import useForm from "../hooks/useForm";
+import { FormOptions } from "../types/Form.type";
+
 const CreateProduct = () => {
-  return <div></div>;
+  const initialFields = {
+    image: null,
+    title: "",
+    categoryCode: "",
+    content: "",
+    regionCode: "",
+    price: "",
+  };
+
+  const formOptions: FormOptions<FormData, Promise<any>> = {
+    apiRequest: postProduct,
+    onSuccess: () => {
+      console.log("Form submitted successfully");
+      // 성공 처리 로직 추가
+    },
+    onFailure: () => {
+      console.log("Form submission failed");
+      // 실패 처리 로직 추가
+    },
+    contentType: "multipart",
+  };
+
+  const { fields, errors, handleChange, handleSubmit } = useForm(
+    initialFields,
+    formOptions
+  );
+
+  const handleButtonClick = () => {
+    handleSubmit();
+  };
+
+  return (
+    <div>
+      <Header>
+        <ButtonWrapper>
+          <button>뒤로가기</button>
+          <button onClick={handleButtonClick}>완료</button>
+        </ButtonWrapper>
+      </Header>
+      <ContentLayout>
+        <Form
+          fields={createProductFormFields}
+          initialFields={fields}
+          handleChange={handleChange}
+          errors={errors}
+        />
+      </ContentLayout>
+    </div>
+  );
 };
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
 
 export default CreateProduct;
